c78cd62243a9cf3b89604302fa610c0f72610dbd
Luca <paesberaldiluca@gmail.com>
2024-05-05 19:53:00 -0300

    Criei as variáveis para a classe Hotel.

--- untitled/src/Entidades/Hotel.java
+++ untitled/src/Entidades/Hotel.java
@@ -1,4 +1,24 @@
 package Entidades;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Hotel {
+    private final Quarto[] quartos;
+    private final List<Hospede> filaEspera;
+    private final List<Hospede> hospedesNoHotel;
+    private final Lock lock;
+
+    public Hotel(int numQuartos) {
+        quartos = new Quarto[numQuartos];
+        for (int i = 0; i < numQuartos; i++) {
+            quartos[i] = new Quarto(i + 1, 4); // Capacidade de 4 hóspedes por quarto
+        }
+        filaEspera = new ArrayList<>();
+        hospedesNoHotel = new ArrayList<>();
+        lock = new ReentrantLock();
+    }
 }
