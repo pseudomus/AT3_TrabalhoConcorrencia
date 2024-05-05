2c568c3ec3ce7e64d21ddca79ca865fbdea86f6b
RoxasTA <victorbr64g@gmail.com>
2024-05-05 20:52:49 -0300

    Recepcionista completo!

--- untitled/src/Entidades/Recepcionista.java
+++ untitled/src/Entidades/Recepcionista.java
@@ -1,4 +1,26 @@
 package Entidades;
 
-public class Recepcionista extends Thread{
+import java.util.Random;
+
+public class Recepcionista extends Thread {
+    private final int id;
+    private final Hotel hotel;
+
+    public Recepcionista(int id, Hotel hotel) {
+        this.id = id;
+        this.hotel = hotel;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Recepcionista " + id + " pronto para receber hóspedes.");
+        while (true) {
+            try {
+                hotel.receberHospede();
+                Thread.sleep(new Random().nextInt(5000)); // Simulando tempo de atendimento
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
