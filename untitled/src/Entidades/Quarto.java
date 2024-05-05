757227247d554a063f4e23b9a58c6824ae9b3aed
Luca <paesberaldiluca@gmail.com>
2024-05-05 19:30:52 -0300

    Criei as variáveis necessárias para a classe Quarto.

--- untitled/src/Entidades/Quarto.java
+++ untitled/src/Entidades/Quarto.java
@@ -1,4 +1,22 @@
 package Entidades;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Quarto {
+    private final int numero;
+    private final int capacidade;
+    private final Lock lock;
+    private boolean disponivel;
+    public static boolean estaLimpo;
+    public Hospede hospede = null;
+    public Quarto(int numero, int capacidade) {
+        this.numero = numero;
+        this.capacidade = capacidade;
+        this.lock = new ReentrantLock();
+        this.disponivel = true;
+        Quarto.estaLimpo = true;
+
+    }
+
 }
