d81e3b4d5f18accc00fe6d24efadd89f48f38b49
Luca <paesberaldiluca@gmail.com>
2024-05-05 20:12:09 -0300

    Criei as variáveis e o construtor para a classe Hospede.

--- untitled/src/Entidades/Hospede.java
+++ untitled/src/Entidades/Hospede.java
@@ -1,4 +1,15 @@
 package Entidades;
 
-public class Hospede extends Thread{
+import javax.swing.*;
+import java.util.Random;
+
+public class Hospede extends Thread {
+    private final int id;
+    private final Hotel hotel;
+    private boolean hospedado = false;
+
+    public Hospede(int id, Hotel hotel) {
+        this.id = id;
+        this.hotel = hotel;
+    }
 }
