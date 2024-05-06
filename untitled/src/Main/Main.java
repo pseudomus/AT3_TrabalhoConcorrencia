67bbfdaf600f100c70bef8653b1e8f639da86fed
RoxasTA <victorbr64g@gmail.com>
2024-05-05 23:26:13 -0300

    Instituição do arquivo Main

--- untitled/src/Main/Main.java
+++ untitled/src/Main/Main.java
@@ -1,7 +1,33 @@
 package Main;
+import Entidades.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        final int NUM_QUARTOS = 10;
+        final int NUM_HOSPEDES = 15;
+        final int NUM_CAMAREIRAS = 10;
+        final int NUM_RECEPCIONISTAS = 5;
 
+        Hotel hotel = new Hotel(NUM_QUARTOS);
+
+        // Criando recepcionistas
+        for (int i = 0; i < NUM_RECEPCIONISTAS; i++) {
+            new Recepcionista(i + 1, hotel).start();
+        }
+
+        // Criando hóspedes
+        for (int i = 0; i < NUM_HOSPEDES; i++) {
+            new Hospede(i + 1, hotel).start();
+        }
+
+        // Criando camareiras
+        for (int i = 0; i < NUM_CAMAREIRAS; i++) {
+            new Camareira(i + 1, hotel).start();
+        }
     }
 }
