aa9b669729bab22b661c20415097324c1da75ab3
RoxasTA <victorbr64g@gmail.com>
2024-05-06 22:11:25 -0300

    Instituição do arquivo Main

--- untitled/src/Entidades/Camareira.java
+++ untitled/src/Entidades/Camareira.java
@@ -1,10 +1,17 @@
 package Entidades;
 import java.util.Random;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+
 public class Camareira extends Thread{
     private final int id;
     private boolean isCleaning;
     private final Hotel hotel;
+    private boolean isAvilible = true;
+    private Quarto beingCleaned;
+    private Lock lock;
 
     public Camareira(int id, Hotel hotel) {
         this.id = id;
@@ -19,6 +26,30 @@ public class Camareira extends Thread{
     public void setIsCleaning(boolean isCleaning){
         this.isCleaning = isCleaning;
     }
+
+    public void setAvilible() {
+        this.isAvilible = !isAvilible;
+    }
+
+    public void liparOQuarto() {
+        lock.lock();
+        setAvilible();
+        this.beingCleaned = hotel.getQuartoSujo();
+        try {
+            if (beingCleaned != null) {
+                beingCleaned.setBeingCleaned(true);
+                System.out.println(getName() + ": O quarta começou a ser limpo" + beingCleaned.getNumero());
+                Thread.sleep(2000);
+                System.out.println("Quarto " + beingCleaned.getNumero() + " limpo.");
+                beingCleaned.setEstaLimpo(false);
+                setAvilible();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        lock.unlock();
+    }
+
 
     @Override
     public void run() {
