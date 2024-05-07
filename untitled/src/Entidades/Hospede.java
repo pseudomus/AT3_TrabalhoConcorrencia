b9d017cf4f311cd093c906f200f5eaec5e6f0dcc
Luca <paesberaldiluca@gmail.com>
2024-05-06 21:11:03 -0300

    Criei variáveis e métodos para a restruturação do código na classe Hospede.

--- untitled/src/Entidades/Hospede.java
+++ untitled/src/Entidades/Hospede.java
@@ -6,11 +6,38 @@ import java.util.Random;
 public class Hospede extends Thread {
     private final int id;
     private final Hotel hotel;
+    private Quarto quarto;
     private boolean hospedado = false;
+    private Thread thread;
+    private Chave chave;
+    private int tentativasReclamacao = 2;
 
     public Hospede(int id, Hotel hotel) {
         this.id = id;
         this.hotel = hotel;
+        this.quarto = null;
+        this.thread = new Thread(this);
+    }
+
+    public void setQuarto(Quarto quarto){
+        this.quarto = quarto;
+    }
+
+    public void setChave(Chave chave) {
+        this.chave = chave;
+    }
+
+    public int getTentativas() {
+        return tentativasReclamacao++;
+    }
+
+    public void decrementarTentativas() {
+        tentativasReclamacao--;
+    }
+
+    public void reclamar() {
+        System.out.println(this.getId() + ": O Hotel não tem quartos disponíveis. Péssimo, não voltarei!");
+        Thread.currentThread().interrupt();
     }
 
     @Override
