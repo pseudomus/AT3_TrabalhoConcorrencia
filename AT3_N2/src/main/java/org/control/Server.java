cea66995c1ee208babae9dd2f003e74d6d118596
Luca Lacerda <lucagabriel520@gmail.com>
2024-06-15 13:12:36 -0300

    added json dependency

---
+++ AT3_N2/src/main/java/org/control/Server.java
@@ -0,0 +1,19 @@
+package org.control;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Server {
+
+}
